a4d9c1e0c92652a7af8ae3e832e06e1f47dbd0cb
ponychan <rzsckbmx@163.com>
2017-10-06 11:42:59 +0800

    3.2.cpp
    调用函数进行进制转换

--- ConsoleApplication3_2/ConsoleApplication32.cpp
+++ ConsoleApplication3_2/ConsoleApplication32.cpp
@@ -1,11 +1,29 @@
 // ConsoleApplication32.cpp: 定义控制台应用程序的入口点。
-//
+//3.2.cpp调用x的n次方函数进行二进制和十进制的转换
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+double power(double x, int n);
 
 int main()
 {
-    return 0;
+	int value = 0;
+	cout << "enter an 8 bit binary number: ";
+	for (int i = 7; i >= 0; i--) {
+		char ch;
+		cin >> ch;
+		if (ch == '1')
+			value += static_cast<int>(power(2, i));//类型转换为整型
+	}
+	cout << "decimal value is" << value << endl;
+	return 0;
 }
 
+double power(double x, int n) {
+	double val = 1.0;
+	while (n--)
+		val *= x;
+	return val;
+}
